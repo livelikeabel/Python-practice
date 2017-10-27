8321bb90c333b48c3c3fd2f0f51686150395afa0
abdl ko <esung1129@gmail.com>
2017-10-27 12:41:21 +0900

    solve algorithm, get_middle01

--- programmers-algorithm/get_middle01.py
+++ programmers-algorithm/get_middle01.py
@@ -3,15 +3,24 @@ Created on 2017. 10. 27.
 
 @author: MacBook Pro
 '''
+
 def string_middle(str):
     if len(str) % 2 == 0 :
         index_midle_number1 = round(len(str)/2) -1
         index_midle_number2 = round(len(str)/2)
-        return str[index_midle_number1], str[index_midle_number2]
+        return str[index_midle_number1] + str[index_midle_number2]
+    elif len(str) == 3:
+        index_midle_number = round(len(str)/2) -1
+        return str[index_midle_number]
+    
     else:
         index_midle_number = round(len(str)/2)
         return str[index_midle_number]
 
-print('input String everything : ')
-print('result : ', string_middle(input()))
+
+for i in range(10):
+    print('input String everything : ')
+    print('result : ', string_middle(input()))
+
+
 
